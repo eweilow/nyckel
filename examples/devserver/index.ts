d3bbb5bd69ba9c235f26d3f52da28faa34b49721
E. Weilow <eweilow@digitalkoppar.se>
2019-06-09 17:42:50 +0200

    Different refresh time

--- examples/devserver/index.ts
+++ examples/devserver/index.ts
@@ -139,7 +139,7 @@ app.use((req, res, next) => {
       }
 
       function isExpired(expires: number) {
-        return Date.now() > expires - 60000;
+        return Date.now() > expires - 10 * 60 * 1000; // Refreshing 10 minutes before token expires give ample time for multiple retries
       }
 
       if (isExpired(session.expires)) {
