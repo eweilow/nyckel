3de408581f0bdaaf7ef9248ae3be8bda641446f9
E. Weilow <eweilow@digitalkoppar.se>
2019-06-13 11:40:17 +0200

    Test concatUrl

--- packages/authentication/utils/concatUrl.ts
+++ packages/authentication/utils/concatUrl.ts
@@ -4,5 +4,7 @@ export function concatUrl(origin: string, path: string) {
   const url = new URL(origin);
   url.pathname = path;
 
-  return url.href;
+  if (!url.protocol) url.protocol = "http";
+
+  return new URL(url.href).href;
 }
